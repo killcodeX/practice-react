68db4dac99b7d7f9697de31de224f092c4e2ea50
Aaquib Ahmed <aaquibskimbox@gmail.com>
2020-02-26 01:30:16 +0530

    used a state

--- src/App.js
+++ src/App.js
@@ -3,12 +3,24 @@ import './App.css';
 import Person from './Person/Person';
 
 class App extends Component {
+
+  state = {
+    persons:[
+      {name : 'Richie', age : 23},
+      {name : 'Aish', age : 28},
+      {name : 'Farhu', age : 25}
+    ]
+  }
+
   render(){
   return (
     <div className="App">
       <h1>Hi, I am React App</h1>
       <h2>This is second heading</h2>
-      <Person/>
+      <button>Switch Name</button>
+      <Person name={this.state.persons[0].name} age={this.state.persons[0].age}/>
+      <Person name={this.state.persons[1].name} age={this.state.persons[1].age}> My hobbies: Sex</Person>
+      <Person name={this.state.persons[2].name} age={this.state.persons[2].age}/>
     </div>
   );
 }
--- src/Person/Person.js
+++ src/Person/Person.js
@@ -1,7 +1,12 @@
 import React from 'react';
 
-const person = () => {
-    return <p>Hi I am Aaquib</p>
+const person = (props) => {
+return (
+    <div>
+        <p>Hi I am {props.name} and I am {props.age} years old !!</p>
+        <p>{props.children}</p>
+    </div>
+)
 };
 
 export default person;
